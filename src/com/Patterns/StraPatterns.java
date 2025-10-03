a12f8ff06ea2b4f955a2983ac3b693f06a130f6c
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-10-03 07:19:09 +0530

    Added Advance Patter (Butterfly)

--- src/com/Patterns/StraPatterns.java
+++ src/com/Patterns/StraPatterns.java
@@ -112,6 +112,35 @@ public class StraPatterns {
 			}
 			System.out.println();
 		}
+		System.out.println();
+		System.out.println("Advance Patterns");
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			int space = 2 * (5 - i);
+			for (int j = 1; j <= space; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			int space = 2 * (5 - i);
+			for (int j = 1; j <= space; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
