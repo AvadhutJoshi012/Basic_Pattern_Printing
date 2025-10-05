e01715b8108b13241103b0f3212c09e7d8117630
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-10-05 22:26:41 +0530

    Sorting Algorithms and Advance Pattern and Java Functions

--- src/com/Patterns/StraPatterns.java
+++ src/com/Patterns/StraPatterns.java
@@ -142,6 +142,85 @@ public class StraPatterns {
 			System.out.println();
 		}
 
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5 - 1; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= 5; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5; j++) {
+				System.out.print("  ");
+			}
+			for (int j = i; j <= 5; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5 - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5 - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5 - 1; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for (int i = 5; i >= 1; i--) {
+			for (int j = i; j <= 5 - 1; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+////		int i;
+////		int j = 0;
+//		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+////		System.out.println(arr[0][1]);
+//		for (int i = 0; i < arr.length; i++) {
+////			System.out.println(arr[i][j]);
+//			for (int j = 0; j < arr[i].length; j++) {
+//				System.out.print(arr[i][j]);
+//			}
+//			System.out.println();
+//		}
+
 	}
 
 }
